afce027f9bb60cd7a1696e3f1c7c264f141bc1db
Itsuki-M <ituki130618aiko@gmail.com>
2024-04-23 11:42:07 +0900

    農水省報告書作成ツール

--- button2_clicked.py
+++ button2_clicked.py
@@ -9,10 +9,7 @@ def button2_clicked(btn_value, pre_week, this_week):
   folderPath = filedialog.askdirectory(initialdir=iDir)
   folders = ['1.北海道', '2.東北', '3.関東','4.北陸', '5.東海', '6.近畿', '7.中四国', '8.九州', '9.沖縄']
   
-  if not check_survey_day(pre_week, this_week, btn_value):
-    return
-  
-  if btn_value == "A":
+  if btn_value == "B":
     nowMonth = this_week
     kiso_Month = int(nowMonth) - 89 if nowMonth[-2:] == "01" else int(nowMonth) - 1
     kiso_day = str(kiso_Month)
@@ -44,17 +41,18 @@ def button2_clicked(btn_value, pre_week, this_week):
         former_file_name = pre_week + '_基礎'
         now_file_name = this_week + 'の週_野菜'
         now_file = former_file.replace(former_file_name, now_file_name)
+      former_file_path = os.path.join(former_folder, former_file)
+      now_file_path = os.path.join(now_folder, now_file)
+      
+      wb = load_workbook(former_file_path)
+      wb.save(now_file_path)
+      if btn_value == "B":
+        kiso_file = now_file.replace(this_week, kiso_day)
+        kiso_file_path = os.path.join(kiso_folder, kiso_file)
+        kiso_wb = load_workbook(kiso_file_path)
+        main_transfer(kiso_wb, wb, btn_value)
+      else:
+        main_transfer(None, wb, btn_value, now_file_path)
     
-    former_file_path = os.path.join(former_folder, former_file)
-    now_file_path = os.path.join(now_folder, now_file)
-    
-    wb = load_workbook(former_file_path)
-    wb.save(now_file_path)
-    if btn_value == "B":
-      kiso_file = now_file.replace(this_week, kiso_day)
-      kiso_file_path = os.path.join(kiso_folder, kiso_file)
-      kiso_wb = load_workbook(kiso_file_path)
-    
-    main_transfer(kiso_wb, wb, btn_value)
     
   messagebox.showinfo('成功', '報告書を作成しました')
